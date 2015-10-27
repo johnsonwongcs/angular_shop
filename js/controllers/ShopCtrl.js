3f71fd4339dbdbc54689b647cdde37fc2f20277b
nadyaPeeps <nadezhda.fed.28@gmail.com>
2015-10-27 15:42:52 -0400

    css + shop template

--- js/controllers/ShopCtrl.js
+++ js/controllers/ShopCtrl.js
@@ -4,7 +4,9 @@ function ShopCtrl(productService, products, $location){
 	this.productService = productService;
 	this.products = products;
 	this.location = $location;
+	this.randomProducts = this.products.slice(0,3);
+	console.log(this.randomProducts);
 }
-ShopCtrl.prototype.goToProduct = function(){
-
+ShopCtrl.prototype.displayRandom = function(){
+    
 }
